d610051f575f9248f617fc0ece4acfa66f1ddebf
Matthieu Dartiailh <marul@laposte.net>
2018-02-15 23:58:34 +0100

    tag 0.1.0 release

--- setup.py
+++ setup.py
@@ -10,9 +10,18 @@ sys.path.insert(0, os.path.abspath('.'))
 from exopy_hqc_legacy.version import __version__
 
 
+def long_description():
+    """Read the project description from the README file.
+
+    """
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+        return f.read()
+
+
 setup(
     name='exopy_hqc_legacy',
     description='Transitional package between HQCMeas and Exopy',
+    long_description=long_description(),
     version=__version__,
     author='see AUTHORS',
     author_email='m.dartiailh@gmail.com',
