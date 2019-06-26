c9236d2b31fbc8a94a76bd119d265f02bbc76b32
Réouven Assouly <reouvenassouly@yahoo.fr>
2019-06-26 15:42:38 +0200

    AWG: use events instead of on/off for easier synchronisation with the acquisition cards
    The code is a slightly modified version of the code written by Raphaël and Zaki.

--- exopy_hqc_legacy/tasks/tasks/instr/run_awg.py
+++ exopy_hqc_legacy/tasks/tasks/instr/run_awg.py
@@ -10,8 +10,10 @@
 
 """
 import logging
+import numbers
+import time
 
-from atom.api import (Unicode, set_default)
+from atom.api import (Float, Unicode, set_default)
 
 from exopy.tasks.api import (InstrumentTask, validators)
 
@@ -22,20 +24,45 @@ class RunAWGTask(InstrumentTask):
     """
     #: Switch to choose the AWG run mode: on or off
     switch = Unicode('Off').tag(pref=True, feval=validators.SkipLoop())
+    delay = Float(0).tag(pref=True,
+                         feval=validators.SkipLoop(types=numbers.Real))
+
     database_entries = set_default({'output': 0})
 
     def perform(self, switch=None):
         """Default interface behavior.
 
         """
+        delay = self.format_and_eval_string(self.delay)
         if switch is None:
             switch = self.format_and_eval_string(self.switch)
 
         if switch == 'On' or switch == 1:
-            self.driver.running = 1
+            print('On')
+            self.driver.send_event()
+            # The delay is required when loading large sequences
+            self.driver.run_awg(1, delay=delay)
             self.write_in_database('output', 1)
+        elif switch == 'Event':
+            print('Event')
+            time.sleep(delay)
+            self.driver.send_event()
+        elif switch == 'Rearm':
+            print('Rearm')
+            time.sleep(delay)
+            success = False
+            while not success:
+                self.driver.send_event()
+                pos = int(self.driver.ask_sequencer_pos())
+                print(pos)
+                if pos == 1:
+                    success = True
+                print(success)
+            time.sleep(delay)
         else:
-            self.driver.running = 0
+            print('Off')
+            time.sleep(delay)
+            self.driver.run_awg(0)
             self.write_in_database('output', 0)
         log = logging.getLogger(__name__)
         msg = 'AWG running state OK'
